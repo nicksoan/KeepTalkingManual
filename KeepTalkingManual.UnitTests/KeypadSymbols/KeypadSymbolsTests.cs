5e5e1b0225d8f64c517ad4f3c788663f4eab2aa8
Nick Soan <nick.soan@googlemail.com>
2023-12-04 19:41:18 +0000

    Add UI for Keypad

--- KeepTalkingManual.UnitTests/KeypadSymbols/KeypadSymbolsTests.cs
+++ KeepTalkingManual.UnitTests/KeypadSymbols/KeypadSymbolsTests.cs
@@ -29,7 +29,20 @@ namespace KeepTalkingManual.UnitTests.KeypadSymbols
         [Theory]
         [KeyPadListDataAttribute("Matching Sets: Set1", choices_KeypadSymbol.balloon,choices_KeypadSymbol.at,choices_KeypadSymbol.squidknife)]
         [KeyPadListDataAttribute("Matching Sets: Set1", choices_KeypadSymbol.leftc, choices_KeypadSymbol.upsidedowny, choices_KeypadSymbol.hookn)]
+        [KeyPadListDataAttribute("Matching Sets: Set1", choices_KeypadSymbol.at, choices_KeypadSymbol.leftc, choices_KeypadSymbol.squigglyn)]
         public void ShouldMatch_Set1(List<choices_KeypadSymbol> keypadSymbols, string expectedResults)
+        {
+            KeypadSymbolModule keypadModule = new KeypadSymbolModule();
+
+            Assert.Equal(expectedResults, keypadModule.GetInstructions(keypadSymbols));
+
+        }
+
+        [Theory]
+        [KeyPadListDataAttribute("Matching Sets: Set2", choices_KeypadSymbol.hollowstar, choices_KeypadSymbol.questionmark, choices_KeypadSymbol.euro)]
+        [KeyPadListDataAttribute("Matching Sets: Set2", choices_KeypadSymbol.leftc, choices_KeypadSymbol.cursive, choices_KeypadSymbol.balloon)]
+        [KeyPadListDataAttribute("Matching Sets: Set2", choices_KeypadSymbol.hookn, choices_KeypadSymbol.balloon, choices_KeypadSymbol.euro)]
+        public void ShouldMatch_Set2(List<choices_KeypadSymbol> keypadSymbols, string expectedResults)
         {
             KeypadSymbolModule keypadModule = new KeypadSymbolModule();
 
